594966c7e17c5f6f8cb22ce5b58906f66e2cc075
muzudho <muzudho1@gmail.com>
2026-03-29 00:23:04 +0900

    WIP とりあえず、コピーして持ってきた。

---
+++ HelloConsoleAppCSharp2/Infrastructure/Configuration/MuzAppSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloConsoleAppCSharp.Infrastructure.Configuration;
+
+/// <summary>
+///     <pre>
+/// appsettings.json ファイルのオブジェクト。
+///     </pre>
+/// </summary>
+public class MuzAppSettings
+{
+    /// <summary>
+    /// アプリケーション名。
+    /// </summary>
+    public string AppName { get; set; } = string.Empty;
+}
--- HelloConsoleAppCSharp2/Program.cs
+++ HelloConsoleAppCSharp2/Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using HelloConsoleAppCSharp;
+using HelloConsoleAppCSharp.Infrastructure.Configuration;
+using HelloConsoleAppCSharp.Infrastructure.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+try
+{
+    Console.WriteLine("Hello, World!");
+
+    // ホストビルドするぜ（＾～＾）！
+    // ［ホスト］ってのは［汎用ホスト］のことで、いろいろ［サービス］っていう便利機能を付け加えることができるフレームワークみたいなもんだぜ（＾～＾）
+    // それを［ビルド］するぜ（＾▽＾）
+    await MuzInfrastructureHelper.BuildHostAsync(
+        commandLineArgs: args,
+        onHostEnabled: async (host) =>
+        {
+            // ここからビルドされた［汎用ホスト］（host）が使えるぜ（＾▽＾）！
+            Console.WriteLine("ここからビルドされた［汎用ホスト］（host）が使えるぜ（＾▽＾）！");
+
+            // ［アプリケーション設定ファイル］を動作確認してみようぜ（＾～＾）
+            var appSettings = host.Services.GetRequiredService<IOptions<MuzAppSettings>>().Value;
+            Console.WriteLine($"AppName: {appSettings.AppName}");
+
+            // ［ロガー］を動作確認してみようぜ（＾～＾）
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("デフォルトのログを書き込むぜ～（＾～＾）！");
+
+            // ［ロガー別のログ］を動作確認してみようぜ（＾～＾）
+            var loggingSvc = host.Services.GetRequiredService<IMuzLoggingService>();
+            loggingSvc.Others.LogInformation("その他のログだぜ（＾～＾）");
+            loggingSvc.Verbose.LogInformation("大量のログだぜ（＾～＾）");
+        });
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"アプリが死んだ... ログも取れない、むずでょ泣く。{ex}");
+}
+finally
+{
+    Console.WriteLine("アプリが終了するぜ（＾～＾）！");
+    await MuzInfrastructureHelper.Cleanup();
+}
+
+// Program.cs を最後まで実行しても、必ずしもアプリケーションが終了するわけじゃないぜ（＾～＾）！
+// ［汎用ホスト］が動いている限りは、アプリケーションは終了しないぜ（＾～＾）！
