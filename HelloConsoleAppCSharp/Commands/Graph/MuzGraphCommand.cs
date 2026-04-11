2be5beddc372c08b4c653fef29b5b91cac52e37e
muzudho <muzudho1@gmail.com>
2026-04-11 12:25:13 +0900

    ［graph］更新

---
+++ HelloConsoleAppCSharp/Commands/Graph/MuzGraphCommand.cs
@@ -0,0 +1,84 @@
+﻿namespace HelloConsoleAppCSharp.Commands.Graph;
+
+using HelloConsoleAppCSharp.Infrastructure.REPL;
+using HelloConsoleAppCSharp.Infrastructure.Scheduler;
+using HelloConsoleAppCSharp.Views;
+using System;
+using static HelloConsoleAppCSharp.Infrastructure.REPL.MuzREPL;
+
+internal static class MuzGraphCommand
+{
+    internal static async Task<MuzRequestType> ExecuteAsync(
+        DateTime startDateTime)
+    {
+        MuzREPL.IsPromptVisible = false; // プロンプトは消す。
+        MuzPageLayouts.PrintTitlePage();
+
+
+        // 📍 NOTE:
+        //
+        //      ここで、ボーダーを表示してみましょう（＾～＾）！
+        //      方眼紙などに画面の想像図を描いてから、位置とサイズを測って、コーディングしましょう（＾～＾）！
+        //
+        MuzBorders.PrintDoubleBorder(
+            left: 20,
+            top: 15,
+            width: 40,
+            height: 5,
+            fgColor: ConsoleColor.Black,
+            bgColor: ConsoleColor.Cyan);
+
+
+        // 📍 NOTE:
+        //
+        //     ここで、メニューを表示してみましょう（＾～＾）！
+        //
+        MuzVMenus.PrintMenu(
+            left: 38,
+            top: 16,
+            items: new[] { "開始", "設定", "終了" },
+            fgColor: ConsoleColor.Black,
+            bgColor: ConsoleColor.Cyan);
+
+
+        // 📍 NOTE:
+        //
+        //      約1/60秒間隔のタイマーを起動するぜ（＾～＾）！
+        //
+        new MuzTimer(TimeSpan.FromMilliseconds(16)).Run(
+            update: async () =>
+            {
+
+
+                // 📍 NOTE:
+                //
+                //      アプリケーション起動からの経過時刻を表示するぜ（＾～＾）！
+                //
+                MuzWidgets.PrintErapsedTime(
+                    label: "Time ",
+                    startDateTime: startDateTime,
+                    left: 62,
+                    top: 0,
+                    fgColor: ConsoleColor.Black,
+                    bgColor: ConsoleColor.Cyan);
+
+
+                // 📍 NOTE:
+                //
+                //      一定間隔で点滅するカーソルを表示するぜ（＾～＾）！
+                //
+                MuzWidgets.PrintBlinkingText(
+                    text: "▶",  // 右向きの三角形は、半角のようだ。
+                    left: 36,
+                    top: 16,
+                    fgColor: ConsoleColor.Blue,
+                    bgColor: ConsoleColor.Cyan,
+                    isVisible: (DateTime.Now.Millisecond / 500) % 2 == 0); // 0.5秒ごとに点滅
+
+                await Task.CompletedTask;
+            });
+
+
+        return MuzREPL.MuzRequestType.None;
+    }
+}
