13ea3ae1aa09b0bfd13a031a2343bbde1d454e67
muzudho <muzudho1@gmail.com>
2026-04-11 22:59:36 +0900

    wip: message-box

--- HelloConsoleAppCSharp/Controls/MuzMessageBoxControl.cs
+++ HelloConsoleAppCSharp/Controls/MuzMessageBoxControl.cs
@@ -1,6 +1,7 @@
 ﻿namespace HelloConsoleAppCSharp.Controls;
 
 using HelloConsoleAppCSharp.Infrastructure.REPL;
+using HelloConsoleAppCSharp.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -39,6 +40,46 @@ internal class MuzMessageBoxControl
 
     public async Task<MuzRequestType> Enter()
     {
+        //              4   8  12  16  20  24  28  32  36  40  44  48  52  56  60  64  68  72  76  80
+        //          +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //        5 +                                                                               +
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //       10 +                                                                               +
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //       15 +                                                                               +
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |                                                                               |
+        //          |╔═════════════════════════════════════════════════════════════════════════════╗|
+        //       20 +║                                                                             ║+
+        //          |║                                                                             ║|
+        //          |║                                                                             ║|
+        //          |║                                                                             ║|
+        //          |║                                                                             ║|
+        //       25 |╚═════════════════════════════════════════════════════════════════════════════╝|
+        //          +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
+        //
+        //      👆　とりあえず、５行表示できるボックスを下の方いっぱいに配置してみるぜ（＾～＾）
+
+        // ボックス表示
+        MuzBoxViews.PrintDoubleBorderBox(
+            left: 0,
+            top: 19,
+            width: 80,
+            height: 7,
+            fgColor: ConsoleColor.Black,
+            bgColor: ConsoleColor.Cyan);
+
         return MuzRequestType.None;
     }
 }
