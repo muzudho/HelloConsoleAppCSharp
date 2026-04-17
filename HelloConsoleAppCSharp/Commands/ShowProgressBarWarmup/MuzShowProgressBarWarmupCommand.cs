679792befec7d1525a689dc8d0b3bc935bd6378d
muzudho <muzudho1@gmail.com>
2026-04-17 20:08:07 +0900

    update progress bar.

--- HelloConsoleAppCSharp/Commands/ShowProgressBarWarmup/MuzShowProgressBarWarmupCommand.cs
+++ HelloConsoleAppCSharp/Commands/ShowProgressBarWarmup/MuzShowProgressBarWarmupCommand.cs
@@ -2,6 +2,7 @@
 
 using HelloConsoleAppCSharp.Infrastructure.ConsoleCustom;
 using HelloConsoleAppCSharp.Infrastructure.REPL;
+using System.Text;
 
 internal static class MuzShowProgressBarWarmupCommand
 {
@@ -9,62 +10,47 @@ internal static class MuzShowProgressBarWarmupCommand
     {
         Console.WriteLine("これから、進捗バーの真似事をするぜ（＾～＾）");
 
-        await Task.Delay(TimeSpan.FromSeconds(1));
+        // コンソール画面の横幅を取得（＾～＾）
+        int consoleWidth = Console.WindowWidth;
 
-        // 処理の後、カーソルを元の位置に戻す
-        await MuzConsoleHelper.ResetCursorLocationAfterExecute(async () =>
+        // "100%" の文字数（＾～＾） まあ、 4 文字だよね（＾～＾）
+        int labelLength = "100%".Length;
+
+        // プログレスバーの長さは、［コンソール画面の横幅］－［"100%" の文字数］とする（＾～＾）
+        int progressBarLength = consoleWidth - labelLength;
+
+        for (int i = 0; i < progressBarLength; i++)
         {
-            // ［進捗バー］幅１
-            await MuzConsoleHelper.SetColorAsync(
-                fgColor: ConsoleColor.White,
-                bgColor: ConsoleColor.Blue,
-                onColorChanged: async () =>
-                {
-                    Console.Write(" ");
-                });
+            // ちょっと待つ
+            await Task.Delay(TimeSpan.FromMilliseconds(100));
 
-            Console.Write("1");
-            Console.Write(" ".PadRight(Console.BufferWidth)); // 残りを空白で消す。カーソルは次の行の先頭へ行く。
-        });
+            // 処理の後、カーソルを元の位置に戻す
+            await MuzConsoleHelper.ResetCursorLocationAfterExecute(async () =>
+            {
+                // ［進捗バー］幅１
+                await MuzConsoleHelper.SetColorAsync(
+                    fgColor: ConsoleColor.White,
+                    bgColor: ConsoleColor.Blue,
+                    onColorChanged: async () =>
+                    {
+                        var sb = new StringBuilder();
+                        for (int j = 0; j < i + 1; j++)
+                        {
+                            sb.Append(" ");
+                        }
+                        Console.Write(sb.ToString());
+                    });
 
-        await Task.Delay(TimeSpan.FromSeconds(1));
-
-        // 処理の後、カーソルを元の位置に戻す
-        await MuzConsoleHelper.ResetCursorLocationAfterExecute(async () =>
-        {
-            // ［進捗バー］幅２
-            await MuzConsoleHelper.SetColorAsync(
-                fgColor: ConsoleColor.White,
-                bgColor: ConsoleColor.Blue,
-                onColorChanged: async () =>
-                {
-                    Console.Write("  ");
-                });
-
-            Console.Write("2");
-            Console.Write(" ".PadRight(Console.BufferWidth));
-        });
-
-        await Task.Delay(TimeSpan.FromSeconds(1));
-
-        // 処理の後、カーソルを元の位置に戻す
-        await MuzConsoleHelper.ResetCursorLocationAfterExecute(async () =>
-        {
-            // ［進捗バー］幅３
-            await MuzConsoleHelper.SetColorAsync(
-                fgColor: ConsoleColor.White,
-                bgColor: ConsoleColor.Blue,
-                onColorChanged: async () =>
-                {
-                    Console.Write("   ");
-                });
-
-            Console.Write("3");
-            Console.Write(" ".PadRight(Console.BufferWidth));
-        });
+                Console.SetCursorPosition(progressBarLength, Console.CursorTop); // 画面の右端に "nnn%" を表示させるぜ（＾▽＾）
+                Console.Write($"{(i + 1) * 100 / progressBarLength,3}%");
+                //Console.Write(" ".PadRight(Console.BufferWidth)); // 残りを空白で消す。カーソルは次の行の先頭へ行く。
+            });
+        }
 
         // まだ［進捗バー］の行の先頭にカーソルがあるので、改行
         Console.WriteLine();
+
+        Console.WriteLine("完了～（＾▽＾）");
 
         return MuzRequestType.None;
     }
